bc8a76bd3076ce55850ccd97a2512342fcb2e27f
wzx <wzx@163.com>
2022-08-16 20:25:18 +0800

    0.0.1

---
+++ springmvc1/src/main/java/wzx/mvc/springmvc1/git/GitTest.java
@@ -0,0 +1,8 @@
+package wzx.mvc.springmvc1.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("gogo");
+        System.out.println("sadhaih");
+    }
+}
